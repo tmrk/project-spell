168643427b98d4589c74fe6d0d6ab42ed721497d
Tamas Marki <tamas.marki@gmail.com>
2023-01-18 15:45:58 +0545

    added sound effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import { useState, useEffect } from 'react';
 import { useSpeechSynthesis } from 'react-speech-kit';
+import useSound from 'use-sound';
 import Letter from './components/Letter';
 import './App.scss';
+import bgMusic from './sounds/bgmusic.mp3';
+import doneSfx from './sounds/done.mp3';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import MusicOffIcon from '@material-ui/icons/MusicOff';
 
 const WORDS = ["cat", "mouse", "horse", "cow", "dog"];
+const MUSIC = true;
 const IS_CASE_SENSITIVE = false;
 
 function App() {
 
   const { speak } = useSpeechSynthesis();
+  const [playDone] = useSound(doneSfx, { volume: 0.8 });
+  //const [playBgMusic, { pause }] = useSound(bgMusic, { volume: 0.75 });
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [playBgMusic, {pause}] = useSound(bgMusic, {
+    onplay: () => setIsPlaying(true),
+    onend: () => setIsPlaying(false)
+  });
+
+  const toggleMusic = () => {
+    if (isPlaying) pause();
+    else playBgMusic();
+    setIsPlaying(!isPlaying);
+  }
 
   const [remainingWords, setRemainingWords] = useState(WORDS);
   const [letters, setLetters] = useState([]);
   const [currentLetterIndex, setCurrentLetterIndex] = useState(null);
+  const [startPage, setStartPage] = useState(true);
 
   const displayNewWord = () => {
       const currentWord = remainingWords[Math.floor(Math.random() * remainingWords.length)];
@@ -29,7 +49,7 @@ function App() {
 
       setLetters(currentWordLetters);
       setCurrentLetterIndex(0);
-      speak({ text: "Now spell the word " + currentWord });
+      speak({ text: "Spell the word " + currentWord });
 
       if (remainingWords.length > 1) setRemainingWords(remainingWords.filter(word => word !== currentWord));
       else setRemainingWords(WORDS);
@@ -44,12 +64,12 @@ function App() {
   }
 
   const wordDone = () => {
+    playDone();
     speak({ text: "Well done!" });
     displayNewWord();
   }
   
   const jumpToNextLetter = () => {
-    //speak({ text: letters[currentLetterIndex].letter });
     const isLastLetter = currentLetterIndex === letters.length - 1;
     if (!isLastLetter) setCurrentLetterIndex(currentLetterIndex + 1);
     else {
@@ -60,16 +80,37 @@ function App() {
   }
 
   const focusInput = () => {
+    if (startPage) setStartPage(false);
     document.getElementsByTagName("input")[0].focus();
   }
 
+  const [pageBackground, setPageBackground] = useState('');
+
+  const attemptSuccess = (isSuccess) => {
+    if (isSuccess) setPageBackground('green');
+    else setPageBackground('red');
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setPageBackground('');
+     }, 150);
+ 
+    return () => clearTimeout(timeout);
+   }, [pageBackground]);
+
   // Start game
   useEffect(() => {
+    if (MUSIC) playBgMusic();
     displayNewWord();
-  }, [])
+  }, [startPage])
 
   return (
-    <div id="app" onClick={focusInput}>
+    <div id="app" onClick={focusInput} className={pageBackground}>
+      <div id="toggle-music" onClick={toggleMusic}>
+        {isPlaying ? <MusicNoteIcon /> : <MusicOffIcon />}
+      </div>
+      {startPage ? <div id="msg">START</div> :
       <div id="word">
       {letters.map(letter => {
         return (
@@ -81,10 +122,12 @@ function App() {
             letters={letters}
             addAttempt={addAttempt}
             IS_CASE_SENSITIVE={IS_CASE_SENSITIVE}
+            attemptSuccess={attemptSuccess}
+            speak={speak}
           />
         )
       })}
-      </div>
+      </div>}
     </div>
   );
 }
--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -1,9 +1,17 @@
-const Letter = ({letters, index, currentLetterIndex, jumpToNextLetter, addAttempt, IS_CASE_SENSITIVE}) => {
+import { useState } from 'react';
+import useSound from 'use-sound';
+import popSfx from '../sounds/pop.mp3';
+import badSfx from '../sounds/bad.mp3';
+
+const Letter = ({letters, index, currentLetterIndex, jumpToNextLetter, addAttempt, IS_CASE_SENSITIVE, attemptSuccess, speak}) => {
 
   const lastAttempt = letters[index].attempts[letters[index].attempts.length - 1];
   const letter = letters[index].letter;
   const isFocused = index === currentLetterIndex;
   const isDone = lastAttempt && ((IS_CASE_SENSITIVE && lastAttempt === letter) || (!IS_CASE_SENSITIVE && lastAttempt.toLowerCase() === letter.toLowerCase()));
+
+  const [playPop] = useSound(popSfx);
+  const [playBad] = useSound(badSfx);
 
   const clearInput = () => {
     document.getElementsByTagName("input")[0].value = "";
@@ -16,15 +24,30 @@ const Letter = ({letters, index, currentLetterIndex, jumpToNextLetter, addAttemp
 
     if ((IS_CASE_SENSITIVE && input === letter ) || (!IS_CASE_SENSITIVE && input.toLowerCase() === letter.toLowerCase())) {
       console.log("Good job!");
+      playPop();
+      attemptSuccess(true);
       jumpToNextLetter();
-      
     } else {
       console.log("Try again");
+      playBad();
+      attemptSuccess(false);
     }
   }
 
+  const [wobble, setWobble] = useState(0);
+
   return (
-    <div className="letter" data-done={isDone} data-focused={isFocused}>
+    <div 
+      className="letter"
+      data-done={isDone} 
+      data-focused={isFocused}
+      onClick={() => {
+        speak({ text: letter.toUpperCase() });
+        setWobble(1);
+      }}
+      onAnimationEnd={() => setWobble(0)}
+      wobble={wobble}
+    >
       <div>{letter}</div>
       {isFocused ? 
         <input type="text" autoFocus onChange={handleChange} onFocus={clearInput}/>
